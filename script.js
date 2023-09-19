c1435ee64f6c943a84ad687b0493ecde48424ee6
Vidhya Parmeshwar Kadam <131935689+vidhyakadam@users.noreply.github.com>
2023-09-19 11:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".btn");
+    const stopButton = document.querySelector(".stop");
+    const audio = document.getElementById("audio");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const soundFile = this.getAttribute("data-sound");
+            audio.src = `sounds/${soundFile}`;
+            audio.play();
+        });
+    });
+
+    stopButton.addEventListener("click", function () {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+});
